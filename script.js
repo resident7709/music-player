6a8793bea03da9707af1b455b169c3c43509ac66
resident7709 <106827895+resident7709@users.noreply.github.com>
2023-02-09 21:55:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,144 @@
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+const music = document.querySelector("audio");
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress");
+const currentTimeEl = document.getElementById("current-time");
+const durationEl = document.getElementById("duration");
+const prevBtn = document.getElementById("prev");
+const playBtn = document.getElementById("play");
+const nextBtn = document.getElementById("next");
+
+// *Music
+const songs = [
+  {
+    name: "song-1",
+    displayName: "Love is All Around",
+    artist: "Wet Wet Wet",
+  },
+  {
+    name: "song-2",
+    displayName: "Sweet Harmony",
+    artist: "The Beloved",
+  },
+  {
+    name: "song-3",
+    displayName: "Hello",
+    artist: "Martin Solveig",
+  },
+  {
+    name: "song-4",
+    displayName: "Spybreak!",
+    artist: "Propellerheads",
+  },
+  {
+    name: "song-5",
+    displayName: "Body Movin'",
+    artist: "Beastie Boys",
+  },
+  {
+    name: "song-6",
+    displayName: "Ya Mama",
+    artist: "Fat Boy Slim",
+  },
+  {
+    name: "song-7",
+    displayName: "We Live Forever",
+    artist: "The Prodigy",
+  },
+];
+
+// *Check if playing
+let isPlaying = false;
+
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace("fa-play", "fa-pause");
+  playBtn.setAttribute("title", "Pause");
+  music.play();
+}
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
+  music.pause();
+}
+
+// *Play or Pause Event Listener
+playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+// *Update DOM
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+}
+
+// *Current Song
+let songIndex = 0;
+
+// *Previous Song
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// *Next Song
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+loadSong(songs[songIndex]);
+
+// *Update Progress Bar & Time
+function updateProgressBar(e) {
+  if (isPlaying) {
+    const { duration, currentTime } = e.srcElement;
+    // Update progress bar width
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+    // Calculate display for duration
+    const durationMinutes = Math.floor(duration / 60);
+    let durationSeconds = Math.floor(duration % 60);
+    if (durationSeconds < 10) {
+      durationSeconds = `0${durationSeconds}`;
+    }
+    // Avoid NaN
+    if (durationSeconds) {
+      durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+    }
+    // Calculate display for current
+    const currentMinutes = Math.floor(currentTime / 60);
+    let currentSeconds = Math.floor(currentTime % 60);
+    if (currentSeconds < 10) {
+      currentSeconds = `0${currentSeconds}`;
+    }
+    currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+  }
+}
+
+// *Set Progress Bar
+function setProgressBar(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const { duration } = music;
+  music.currentTime = (clickX / width) * duration;
+}
+
+// *Event Listeners
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+music.addEventListener("ended", nextSong);
+music.addEventListener("timeupdate", updateProgressBar);
+progressContainer.addEventListener("click", setProgressBar);
